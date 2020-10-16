fdc5b441c19b9ccdfb53682634c072330e75554e
Lessleya <lesliee.icc@gmail.com>
2020-10-16 13:58:49 -0600

    les

---
+++ cit160/js/poundsToKilograms.js
@@ -0,0 +1,15 @@
+function convKg() {
+
+
+    //INPUT:  Read a pounds number from the user.
+    let pounds = document.getElementById("pounds").value;
+    //PROCESSING convert the string to numbers using parseFloat
+    pounds = parseFloat(pounds);
+    //Convert the number of pounds into Kilograms by dividing pounds / 2.205
+    let kilos = pounds / 2.205;
+
+
+    //OUTPUT: Display the number of kilograms
+
+    document.getElementById("output").innerHTML = pounds + " <strong> pounds </strong> equal to " + kilos.toFixed(1) + "<strong> kilos</strong>.";
+}
