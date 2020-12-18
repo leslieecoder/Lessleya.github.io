bdf72ececd1331e481d326aba7a8209364c97aa8
Lessleya <lesliee.icc@gmail.com>
2020-12-18 14:00:10 -0700

    Last

--- cit160/js/loan.js
+++ cit160/js/loan.js
@@ -1,54 +1,50 @@
-function selector() {
-    let select = document.getElementById("selector").value;
-    if (select == 'Payment') {
-        function doPayment()
-    } else {
-        function doBalance()
-    }
-}
+//Take no parameters.
+//Are called from an onclick attribute.
+//Get input from the user.
+//Call the computePayment or the computeBalance function.
+//Display a result to the user.
 
 function doPayment() {
-    /*Input*/
-    let amountB = parseFloat(document.getElementById("amountB").value);
-    let annualR = parseFloat(document.getElementById("annualR").value);
-    let numberYears = parseFloat(document.getElementById("numberyears").value);
-    let PerYear = parseFloat(document.getElementById("PerYear").value);
-
-    //Process
-    let payment = computePayment(amountB, annualR, numberYears, PerYear);
-    let p = Payment.toFixed(2);
-
-    //Output
-    document.getElementById('output').innerHTML = '$' + p;
+    let principal = parseFloat(document.getElementById("principal").value);
+    let rate = parseFloat(document.getElementById("rate").value);
+    let years = parseFloat(document.getElementById("years").value);
+    let periods = parseFloat(document.getElementById("periods").value);
+    let message1 = "";
+    let pay = computePayment(principal, rate, years, periods);
+    message1 = `Your payment will be ${pay}`;
+    document.getElementById('output1').innerHTML = message1
 }
 
-function computePayment(a, annualR, numYears, PerYear) {
-    //process for calcuations
-    let r = annualR / 12;
-    let n = numberYears * PerYear;
-    let f = a * Math.pow((1 + r), n);
-    return f;
+//computes and returns the monthly payment for a loan with a fixed annual interest rate. 
+//The formula for computing a loan payment is:
+// p =  ar / 1 − (1 + r)^−n
+// a = the loan amount
+// r = rate per period
+// n = total # of periods throughout the life of the loan 
+// y = number of years
+function computePayment(a, r, y, n) {
+    return (a * (r / n) / (1 - ((1 + (r / n)) ** (-n * y)))).toFixed(2);
 }
 
 function doBalance() {
-    //Input
-    let amountBorrowed = parseFloat(document.getElementById("amountB").value);
-    let annualRate = parseFloat(document.getElementById("annualR").value);
-    let numOfYears = parseFloat(document.getElementById("numberYears").value);
-    let periodsPerYear = parseFloat(document.getElementById("PerYear").value);
-
-    //Process
-    let Balance = computeBalance(amountB, annualR, numberYears, PerYear);
-    let b = Balance.toFixed(2);
-
-    //Output
-    document.getElementById('output').innerHTML = '$' + b;
+    let principal = parseFloat(document.getElementById("principal").value);
+    let rate = parseFloat(document.getElementById("rate").value);
+    let numPaid = parseFloat(document.getElementById("payments").value);
+    let periods = parseFloat(document.getElementById("periods").value);
+    let years = parseFloat(document.getElementById("years").value);
+    let message2 = "";
+    let balance = computeBalance(principal, rate, years, periods, numPaid);
+    message2 = `Your balance is ${balance}`;
+    document.getElementById('output2').innerHTML = message2
 }
 
-function computeBalance(a, annualR, numberYears, PerYear) {
-    //process for the calcuations
-    let r = annualR / 12;
-    let n = numberYears * PerYear;
-    let f = a * Math.pow((1 + r), n);
-    return f;
+//Formula: b = a (1 + r)^d − [ p ( (1 + r)^d − 1 ) /r]
+// a = loan amount
+// r = rate per period
+// d = number of payments
+// y = number of years
+
+function computeBalance(a, r, y, n, d) {
+    let monthly = a * (r / n) / (1 - ((1 + (r / n)) ** (-n * y)));
+    return (a * Math.pow(1 + (r / n), d) - (monthly * (Math.pow(1 + (r / 12), d) - 1)) / (r / n)).toFixed(2);
 }
