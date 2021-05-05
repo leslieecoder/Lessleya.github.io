bbfcf9ca79c5ce91bb7ed4323ac0f78c016d345d
Lessleya <lesliee.icc@gmail.com>
2021-05-05 15:08:12 -0600

    Updated

--- CIT111/Week 01/tire_volume.py
+++ CIT111/Week 01/tire_volume.py
@@ -12,3 +12,17 @@ v = round((math.pi * w**2 * a * (w*a+(2540*d)))/10000000,1)
 #Output the result
 print('The approximate volume is', v , "milliliters")
 
+# Import the datatime module so that
+# it can be used in this program.
+from datetime import datetime
+
+# Call the now() method to get the current date and
+# time as a datetime object from the computer's clock.
+current_date_and_time = datetime.now()
+
+# Open a text file named dimensions.txt in append mode.
+with open("volumes.txt", "at") as volumes_file:
+
+    # Print a car's model and dimensions to the file
+    print(f"{current_date_and_time}, {w}, {a}, {d}, {v}", file=volumes_file)
+
